044f7b9c0777c3d2980be1a4fbe67bcbc7a2a54e
merss19 <merss19@gmail.com>
2016-07-04 13:18:35 +0300

    carousel

--- markup/components/content-block/content-block.js
+++ markup/components/content-block/content-block.js
@@ -1,24 +1,52 @@
 
 // reviews
-let reviews = $('.review__user');
+var reviews = $('.review__user');
 
 reviews .owlCarousel({
     items: 1,
     pagination: true,
     singleItem: true,
-    paginationSpeed: 400
+    paginationSpeed: 400,
+    beforeInit: function(){
+        var items = $('.review__user-item');
+        $('.owl-sum').text(items.length);
+        console.log(items.length);
+    }
 });
 
 $('.review__nav-left').click(function () {
-    let $that = $(this),
+    var $that = $(this),
         parent = $that.parent(),
-        carousel = parent.siblings('.owl-carousel');
-    carousel.trigger('owl.next');
+        carousel = parent.siblings('.owl-carousel'),
+        sum = $('.owl-sum').text(),
+        items = carousel.find('.owl-item').length,
+        itemsLen = items.length,
+        current = $('.owl-current').text();
+    carousel.trigger('owl.prev');
+
+    current--;
+    if(current >= 1){
+        $('.owl-current').text(current);
+    } else {
+        $('.owl-current').text(sum);
+    }
+
 });
 $('.review__nav-right').click(function () {
-    let $that = $(this),
+    var $that = $(this),
         parent = $that.parent(),
-        carousel = parent.siblings('.owl-carousel');
-    carousel.trigger('owl.prev');
+        carousel = parent.siblings('.owl-carousel'),
+        sum = $('.owl-sum').text(),
+        items = carousel.find('.owl-item').length,
+        current = $('.owl-current').text();
+    carousel.trigger('owl.next');
+    console.log(items);
+    current++;
+    if(current <= items){
+        console.log(current);
+        $('.owl-current').text(current);
+    } else {
+        $('.owl-current').text(1);
+    }
 });
 
--- markup/components/filter-main/filter-main.js
+++ markup/components/filter-main/filter-main.js
@@ -1,5 +1,10 @@
 
-    $('select').select2({
+  /*  $('select').select2({
         width: '100%'
-    });
+    });*/
 
+    $(document).ready(function() {
+        $(".js-example-basic-single").select2({
+            width: '100%'
+        });
+    });
